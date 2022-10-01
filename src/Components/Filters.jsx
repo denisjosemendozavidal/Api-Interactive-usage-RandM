181b88b238d82d4d43eee3373dc6e1d182ad348d
denisjosemendozavidal <denismendozavidal@gmail.com>
2022-10-01 00:23:36 -0500

    Updating

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Filters = ({sugList}) => {
+
+    const [showingTitle, setShowingTitle] = useState()
+
+    useEffect (() => {
+        if (sugList) {
+            setShowingTitle("Or pick from any of these:")
+        } else {
+            setShowingTitle("")
+        }
+
+    }, [sugList])
+
     
     return (
       <ul className='suglist'>
+      <p className='suglist-tittle'>{showingTitle}</p>
           {
               sugList?.map(location => (
                   <li key={location.id}>{`${location.name}`}</li>
